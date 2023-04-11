c46bb4d34531acba5acdd816f21ce48e624c8494
Mark Harmon <neothej1sec@gmail.com>
2023-04-11 17:40:28 -0500

    alt parser opt

--- lessons/str-2/str5.py
+++ lessons/str-2/str5.py
@@ -176,5 +176,74 @@ class ExpressionTreeBuilder(ExpressionEvaluator):
 # print(e.parse('2 + 3 * 4'))
 # print(e.parse('2 + (3 + 4) * 5'))
 # print(e.parse('2 + 3 + 4'))
+'''
+That being said there are pitfalls
+to hand written parsers and they can
+get sometimes quite complex. For
+more complex parsing use:
+'''
+# Need pip install
+# from ply.lex import lex
+# from ply.yacc import yacc
+
+# tokens = ['NUM', 'PLUS', 'MINUS', 'TIMES', 'DIVIDE', 'LPAREN', 'RPAREN']
+# t_ignore = '\t\n'
+
+# t_PLUS = r'\+'
+# t_MINUS = r'-'
+# t_TIMES = r'\*'
+# t_DIVIDE = r'/'
+# t_LPAREN = r'\('
+# t_RPAREN = r'\)'
+
+# def t_NUM(t):
+#     r'\d+'
+#     t.value = int(t.value)
+#     return t
+# # Error handler
+# def t_error(t):
+#     print('Bad character: {!r}'.format(t.value[0])) t.skip(1)
+# # Build the lexer
+# lexer = lex()
+# # Grammar rules and handler functions
+# def p_expr(p): 
+#     '''
+#     expr : expr PLUS term
+#          | expr MINUS term
+#     '''
+#     if p[2] == '+':
+#         p[0] = p[1] + p[3]
+#     elif p[2] == '-':
+#         p[0] = p[1] - p[3]
+# def p_expr_term(p): 
+#     '''
+#     expr : term '''
+#     p[0] = p[1]
+# def p_term(p): 
+#     '''
+#     term : term TIMES factor
+#          | term DIVIDE factor
+#     '''
+#     if p[2] == '*':
+#         p[0] = p[1] * p[3]
+#     elif p[2] == '/':
+#         p[0] = p[1] / p[3]
+# def p_term_factor(p): 
+#     '''
+#     term : factor '''
+#     p[0] = p[1]
+# def p_factor(p): 
+#     '''
+#     factor : NUM'''
+#     p[0] = p[1]
+# def p_factor_group(p): 
+#     '''
+#     factor : LPAREN expr RPAREN '''
+#     p[0] = p[2]
+# def p_error(p): 
+#     print('Syntax error')
+# parser = yacc()
+# print(parser.parse('2+(3+4)*5'))
+
 # ######## 2.20 ###########
 ''''''
